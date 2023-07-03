948bdade2390e636cef345410d1de7ac90e7c58f
maxling92 <81459084+maxling92@users.noreply.github.com>
2023-07-03 16:59:27 +0700

    Add files via upload

---
+++ Tugas 5/runserverprocess.sh
@@ -0,0 +1,7 @@
+#jalankan 4 process_server
+
+python3 server_process_pool_http.py 9012 &
+python3 server_process_pool_http.py 9013 &
+python3 server_process_pool_http.py 9014 &
+python3 server_process_pool_http.py 9015 &
+
